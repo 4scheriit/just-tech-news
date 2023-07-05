475fd3b488b7eb1e2214a5429d8e563645e21cac
Maxwell Brown <mbrownet@gmail.com>
2023-07-05 11:03:25 -0400

    created a single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -98,13 +98,19 @@ router.post("/", (req, res) => {
 
 // Upvote a post
 router.put("/upvote", (req, res) => {
-  // Call the custom static method "upvote" defined in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Update a post by id
